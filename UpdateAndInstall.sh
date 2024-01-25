5e53bd52436f864ec16226aadada9d6f5c1a29ad
Najshree <119293730+Najshree@users.noreply.github.com>
2024-01-25 11:36:52 -0600

    Added the yml files and remove unecessary code from UpdateAndInstall

--- UpdateAndInstall.sh
+++ UpdateAndInstall.sh
@@ -13,23 +13,9 @@ bash ~/miniconda3/miniconda.sh -b -u -p ~/miniconda3
 
 rm -rf ~/miniconda3/miniconda.sh
 
-if [ -f path/to/users/Shree777/miniconda3/bin/conda ];
+if [ -f /users/Shree777/miniconda3/bin/conda ];
 then
-    /path/to/miniconda3/bin/conda init bash
-~/miniconda3/bin/conda init bash
+    /users/Shree777/miniconda3/bin/conda init bash
+else
+    echo "Not install"
 fi
-source ~/.bashrc
-
-conda --version
-
-conda create --name MyPython python=3.9
-
-conda activate MyPython
-
-conda install -c conda-forge notebook
-
-conda install -c conda-forge nb_conda_kernels
-
-conda install nb_conda
-
-conda env export > requirements.yml
